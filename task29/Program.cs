3be04149f012715ab5e3dabf96306604cac9e6ff
Boris Dovbysh <boris.dovbysh@gmail.com>
2022-11-24 18:38:15 +0300

    add task 29

---
+++ task29/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите количество элементов в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+for(int i = 0; i < N; i++)
+{
+    Console.Write($"Введите {i+1}-й элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+string otvet = string.Join(",", array);
+Console.Write("[");
+Console.Write(otvet);
+Console.Write("]");
