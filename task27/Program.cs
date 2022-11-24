e885def8fbf712f1b21ada7d5be7fb479a2920ca
Boris Dovbysh <boris.dovbysh@gmail.com>
2022-11-24 18:13:36 +0300

    add task 27

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int b = number.Length;
+int result = 0;
+for (int i = 0; i < b ; i++)
+{
+    Console.WriteLine(result);
+    int a = Convert.ToInt32(number[i]);
+    Console.WriteLine(a);
+    result = result + a;
+    Console.WriteLine(number[i]);
+    //Console.WriteLine(result);
+}
+Console.WriteLine($"Сумма цифр числа {number} = {result}");
